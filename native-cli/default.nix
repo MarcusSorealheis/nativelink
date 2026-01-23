3de3f86237464a5e89a254481ced16b47d6867f9
Tom Parker-Shemilt <tom@tracemachina.com>
2026-01-23 18:07:52 +0000

    Fix native-cli deps

--- native-cli/default.nix
+++ native-cli/default.nix
@@ -9,7 +9,7 @@ buildGoModule {
   pname = "native-cli";
   version = "0.6.0";
   src = ./.;
-  vendorHash = "sha256-LOT7keHkGLa+VBdkJLC0cUIN9atYkLaheofQpjF31DE=";
+  vendorHash = "sha256-NePX6xJFHu8e8U4iovgvkDc7BoI9ZspAASr/F7l/ZRA=";
   buildInputs = [makeWrapper];
   ldflags = ["-s -w"];
   installPhase = ''
